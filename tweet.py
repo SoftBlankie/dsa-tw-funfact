6c78905366aca928b389b86c102a6f79541d5398
Derick Phan <dtph@ucdavis.edu>
2020-10-13 16:57:42 -0700

    first commit

--- tweet.py
+++ tweet.py
@@ -2,4 +2,13 @@ import tweepy
 from config import create_api
 
 tw = create_api()
+
+# create tweet by matching current date with dict date
+
+# place status in tweet
+# tweet = tw.update_status('')
+# place tags in comment
+# tweetId = tweet.id_str
+# tw.update_status(status = '', in_reply_to_status_id = tweetId, auto_populate_reply_metadata=True)
+
 #tw.update_status("Hello World!")
